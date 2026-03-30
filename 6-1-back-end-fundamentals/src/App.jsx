eb798c7adcf29256a0492aa532bd83a240cff359
airqx <s202270440@kfupm.edu.sa>
2026-03-30 08:52:18 +0300

    to do 8-9

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -360,6 +360,7 @@ export default function App() {
   // Create state to store student data
   // Syntax hint:
   // const [student, setStudent] = useState(____);
+  const [student, setStudent] = useState(null);
 
   // TODO 8:
   // Request student data from the server when the page loads
@@ -369,6 +370,11 @@ export default function App() {
   //   .then((data) => {
   //     setStudent(data);
   //   });
+  fetch('http://localhost:3000/student')
+    .then((res) => res.json())
+    .then((data) => {
+      setStudent(data);
+    });
 
   return (
     <main className="app-shell">
